e644ec8ea7e011004eccfc62ae1ac1e886dc8e46
PratikNandan <pratikdnandan568@gmail.com>
2024-06-17 16:39:25 +0530

    Adding files

--- darkmode.js
+++ darkmode.js
@@ -1,10 +1,38 @@
+///<reference types="Cypress"/>
+///<reference types='@cypress/xpath'/>
 describe('darkmode', () => {
-    it('check', () => {
-        cy.visit('https://cook-book-pied.vercel.app/').click()
+    it.only('check', () => {
+       // cy.visit('https://cook-book-pied.vercel.app/').click()
+       // cy.xpath("//button[@class='chakra-button css-15w88gn']").click()    //temp working
 
-        cy.xpath("//button[@class='chakra-button css-15w88gn']").click()
+        cy.visit('https://cook-book-pied.vercel.app/');
 
         
+        cy.xpath('(//button[@type="button"])[1]').should('exist').click({force: true})
+        
+
+
+        
+
+    });
+
+    it('dm', () => {
+        cy.visit('https://cook-book-pied.vercel.app/');
+        
+        cy.xpath('(//button[@type="button"])[1]').should('exist').click({force: true})
+        
+        cy.get('body').should('have.class', 'dark-mode');
+    });
+
+    it.skip('login', () => {
+        //Cypress.on('uncaught:exception', (err, runnable) => {
+                 //  return false;
+         // });
+          cy.visit('https://cook-book-pied.vercel.app/')
+          cy.wait(4000)
+          cy.xpath("(//button[@class='chakra-button css-15w88gn'])[1]").click()
+          cy.wait(4000)
+    
 
     });
 });
--- login.js
+++ login.js
@@ -2,13 +2,19 @@ describe('Login', () => {
     it('first', () => {
        
        cy.visit('https://cook-book-pied.vercel.app/').click()
+       //cy.xpath("//button[normalize-space()='LogIn']").click()
+       cy.contains('LogIn').click()
+       cy.xpath("//input[@placeholder='Email']").type('pratik@gmail.com')
+       cy.xpath("//input[@placeholder='Password']").type('1234')
+       cy.xpath("//button[@type='submit']").click()
 
-        cy.xpath("//button[normalize-space()='LogIn']").click()
 
-        cy.xpath("//input[@placeholder='Email']").type('pratik@gmail.com')
-        cy.xpath("//input[@placeholder='Password']").type('1234')
 
-        cy.xpath("//button[@type='submit']").click()
+
+
+
+
+
         
 
 
